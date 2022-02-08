489ebfb9872f94483e39a41404f60e86abb7aeb8
linar777 <linar369@mail.ru>
2022-02-08 21:41:22 +0300

    add reg page

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -5,24 +5,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
 
 namespace App1
 {
+
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void BLogin_Clicked(object sender, EventArgs e)
-        {
 
         }
 
-        private void BReg_Clicked(object sender, EventArgs e)
+        private async void BLogin_Clicked(object sender, EventArgs e)
         {
+           // await Navigation.PushAsync(new RegisterPage());
+        }
 
+        private async void BReg_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegPage());
         }
     }
 }
