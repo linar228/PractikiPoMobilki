5758915df2846e78898d8699f02751446c9c69d1
linar777 <linar369@mail.ru>
2022-02-10 18:12:44 +0300

    repair TabsPage

--- App1/App1/Pages/ContactProjectPage.xaml.cs
+++ App1/App1/Pages/ContactProjectPage.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace App1.Pages
+namespace App1
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ContactProjectPage : ContentPage
--- App1/App1/Pages/ProjectPage.xaml.cs
+++ App1/App1/Pages/ProjectPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App1.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,7 @@ namespace App1
 
         private async void Project_1_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new MainPage());
+            await Navigation.PushAsync(new TabsPage());
         }
     }
 }
