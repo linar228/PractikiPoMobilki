d0f021db727bbbd53bc74d2b5e90e95b9dc58a56
linar777 <linar369@mail.ru>
2022-02-13 20:21:06 +0300

    add SQLITE and upgrade MainPage

--- App1/App1/App.xaml.cs
+++ App1/App1/App.xaml.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using App1.SQLITE;
+using System.IO;
 
 namespace App1
 {
     public partial class App : Application
     {
+        public const string DATABASE_NAME = "Project.db";
+        internal static SQLProject db;
+        internal static SQLProject Db
+        {
+            get
+            {
+                if (db == null)
+                {
+                    db = new SQLProject(
+                        Path.Combine(
+                            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME));
+                }
+                return db;
+            }
+        }
         public App()
         {
             InitializeComponent();
---
+++ App1/App1/SQLITE/SQLProject.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace App1.SQLITE
+{
+    class SQLProject
+    {
+        SQLiteConnection db;
+        
+        public SQLProject(string dbPath)
+        {
+            db = new SQLiteConnection(dbPath);
+            db.CreateTable<Project>();
+        }
+
+
+        public IEnumerable<Project> GetItems()
+        {
+            return db.Table<Project>().ToList();
+        }
+
+
+        public Project GetItem(int id)
+        {
+            return db.Get<Project>(id);
+        }
+
+
+        public int DeleteItem(int id)
+        {
+            return db.Delete<Project>(id);
+        }
+
+        public int SaveItem(Project item)
+        {
+            if (item.Id != 0)
+            {
+                db.Update(item);
+                return item.Id;
+            }
+            else
+            {
+                return db.Insert(item);
+            }
+        }
+    }
+}
