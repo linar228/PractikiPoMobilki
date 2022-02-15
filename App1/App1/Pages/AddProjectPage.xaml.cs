f0301b99d2eac89bfb2b52b562012c66bd4aa87b
linar777 <linar369@mail.ru>
2022-02-16 01:45:07 +0300

    adding a project added

--- App1/App1/Pages/AddProjectPage.xaml.cs
+++ App1/App1/Pages/AddProjectPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using App1.Pages;
+using App1.SQLITE;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +17,27 @@ namespace App1.Pages
         public AddProjectPage()
         {
             InitializeComponent();
+        }
+
+        private async void BRemove_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private async void BAddProjece_Clicked(object sender, EventArgs e)
+        {
+            bool result = await DisplayAlert("Подтвердить действие", $"Вы точно хотите добавить {PName.Text}?", "Да", "Нет");
+            if (result == true)
+            {
+                Project project = new Project();
+                project.Name = PName.Text;
+                project.PTitle = Title.Text;
+                project.Email = Email.Text;
+                project.Phone = PhoneOne.Text;
+                project.Address = Address.Text;
+                App.Db.SaveItem(project);
+                Navigation.PopAsync();
+            }
         }
     }
 }
