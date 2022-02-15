bf1dbabaf041f0ea0f79d1a49ab62263aba1892d
linar777 <linar369@mail.ru>
2022-02-16 01:58:57 +0300

    editing page ready

--- App1/App1/Pages/EditProjectPage.xaml.cs
+++ App1/App1/Pages/EditProjectPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using App1.SQLITE;
+using App1.Pages;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +17,34 @@ namespace App1.Pages
         public EditProjectPage()
         {
             InitializeComponent();
+        }
+
+        private async void BRemove_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private async void BAddProjece_Clicked(object sender, EventArgs e)
+        {
+            var project = (Project)BindingContext;
+            if (await DisplayAlert(" ", $"Вы хотите изменить {project.Name}?", "Изменить", "Отмена"))
+            {
+                if (!String.IsNullOrEmpty(project.Name))
+                {
+                    App.Db.SaveItem(project);
+                }
+                await Navigation.PushAsync(new ProjectPage());
+            }
+        }
+
+        private async void TEditingProject_Clicked(object sender, EventArgs e)
+        {
+            var project = (Project)BindingContext;
+            if (await DisplayAlert(" ", $"Вы точно хотите удалить {project.Name}?", "Удалить", "Отмена"))
+            {
+                App.Db.DeleteItem(project.Id);
+                await Navigation.PushAsync(new ProjectPage());
+            }
         }
     }
 }
